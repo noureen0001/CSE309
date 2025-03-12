2113a43cd79d8a463d2dab79159202349e02dc7c
noureen0001 <2320608@iub.edu.bd>
2025-03-12 10:35:26 +0600

    updated code

--- backend/register.php
+++ backend/register.php
@@ -16,52 +16,5 @@
 
         header('Location: ../login.html');
 
-        // if (strtolower($user_type) === "farmer") {
-        //     $sql1 = "INSERT INTO farmer_t(FarmerID, FName, LName, AreaName, District, Division, ContactNumber) VALUES('$userid', '$first_name', '$last_name', '$area', '$district', '$division' '$contact_number', 'NULL')";
-
-        //     if ($conn->query($sql1) === TRUE) {
-        //         echo "Data updated successfully!";
-        //         header('Location: ../login.html');
-        //     }
-        //     else {
-        //         echo $conn->query($sql1);
-        //     }
-        // } 
-        
-        // else if (strtolower($user_type) === "driver") {
-        //     $sql1 = "INSERT INTO driver_t VALUES('$userid', '$first_name', '$last_name', '$area', '$district', '$division', '$contact_number', NULL, NULL)";
-
-        //     if ($conn->query($sql1) === TRUE) {
-        //         echo "Data updated successfully!";
-        //         header('Location: ../login.html');
-        //     }
-        //     else {
-        //         echo $conn->query($sql1);
-        //     }
-        // }
-
-        // else if (strtolower($user_type) === "warehouse manager") {
-        //     $sql1 = "INSERT INTO warehouse_manager_t VALUES('$userid', '$first_name', '$last_name', '$area', '$district', '$division', '$contact_number')";
-
-        //     if ($conn->query($sql1) === TRUE) {
-        //         echo "Data updated successfully!";
-        //         header('Location: ../login.html');
-        //     }
-        //     else {
-        //         echo $conn->query($sql1);
-        //     }
-        // }
-
-        // else if (strtolower($user_type) === "supplier") {
-        //         $sql1 = "INSERT INTO supplier_t VALUES('$userid', '$user_name', '$area', '$district', '$division', '$contact_number')";
-    
-        //         if ($conn->query($sql1) === TRUE) {
-        //             echo "Data updated successfully!";
-        //             header('Location: ../login.html');
-        //         }
-        //         else {
-        //             echo $conn->query($sql1);
-        //         }
-        // }
     }
 ?>
